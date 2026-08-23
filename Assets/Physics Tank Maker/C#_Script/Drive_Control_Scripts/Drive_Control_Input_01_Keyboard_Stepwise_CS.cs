fe7a22d74fb046a943c20fbfbc12b5690b8bdc03
bossSRS <38173707+bossSRS@users.noreply.github.com>
2026-08-23 15:00:04 +0600

    Add project assets batch 24 of 41 (~199.83 MB)

---
+++ Assets/Physics Tank Maker/C#_Script/Drive_Control_Scripts/Drive_Control_Input_01_Keyboard_Stepwise_CS.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using UnityEngine;
+using CHS.Input;
+using CHS;
+using Sirenix.OdinInspector;
+namespace ChobiAssets.PTM
+{
+
+    public class Drive_Control_Input_01_Keyboard_Stepwise_CS : Drive_Control_Input_00_Base_CS
+    {
+        [ShowInInspector]
+        protected float vertical;
+        [ShowInInspector] 
+        protected float horizontal;
+
+        protected float brakingTime = 0.25f;
+        protected int reverseStepCount = 2;
+        protected int forwardStepCount = 4;
+
+        public CHS_Input chs_Input;
+        public CHS_Controller chs_Controller;
+
+        public void Start()
+        {
+            chs_Input = FindAnyObjectByType<CHS_Input>();
+            chs_Controller = FindAnyObjectByType<CHS_Controller>();
+        }
+        public override void Drive_Input()
+        {
+            if (chs_Input == null) chs_Input = Object.FindAnyObjectByType<CHS_Input>();
+            if (chs_Controller == null) chs_Controller = Object.FindAnyObjectByType<CHS_Controller>();
+
+            if (chs_Controller != null && chs_Controller.harvestorState == HarvestorState.Off)
+            {
+                vertical = 0f;
+                horizontal = 0f;
+                controlScript.Apply_Brake = true;
+                Set_Values();
+                return;
+            }
+
+            bool fwdKey = Input.GetKey(General_Settings_CS.Drive_Up_Key) || Input.GetKey(KeyCode.UpArrow);
+            bool backKey = Input.GetKey(General_Settings_CS.Drive_Down_Key) || Input.GetKey(KeyCode.DownArrow);
+
+            float drivePower = 0f;
+            if (fwdKey)
+            {
+                drivePower = 1.0f;
+            }
+            else if (backKey)
+            {
+                drivePower = -1.0f;
+            }
+            else if (chs_Controller != null && Mathf.Abs(chs_Controller.TempAccleration) > 0.02f)
+            {
+                drivePower = chs_Controller.TempAccleration;
+            }
+            else if (chs_Input != null)
+            {
+                if (chs_Input.mainTransmissionLever == MainTransmissionLever.Increaseing) drivePower = 1.0f;
+                else if (chs_Input.mainTransmissionLever == MainTransmissionLever.Decreaseing) drivePower = -1.0f;
+            }
+
+            if (chs_Input != null && chs_Input.subTransmissionLever == SubTransmissionLever.Neutral && !fwdKey && !backKey)
+            {
+                drivePower = 0f;
+            }
+
+            vertical = drivePower;
+
+            // Set "horizontal" (steering)
+            bool leftKey = Input.GetKey(General_Settings_CS.Drive_Left_Key) || Input.GetKey(KeyCode.LeftArrow) || (chs_Input != null && chs_Input.steering_UpDownLeftRight == SteeringUpDownLeftRight.Left);
+            bool rightKey = Input.GetKey(General_Settings_CS.Drive_Right_Key) || Input.GetKey(KeyCode.RightArrow) || (chs_Input != null && chs_Input.steering_UpDownLeftRight == SteeringUpDownLeftRight.Right);
+
+            if (leftKey)
+            {
+                horizontal = -1.0f;
+            }
+            else if (rightKey)
+            {
+                horizontal = 1.0f;
+            }
+            else
+            {
+                horizontal = 0.0f;
+            }
+
+            // Control the brake.
+            controlScript.Apply_Brake = Input.GetKey(General_Settings_CS.Drive_Brake_Key);
+
+            // Set the "Stop_Flag", "L_Input_Rate", "R_Input_Rate" and "Turn_Brake_Rate".
+            Set_Values();
+        }
+
+
+        protected virtual void Set_Values()
+        {
+            // In case of stopping.
+            if (vertical == 0.0f && horizontal == 0.0f)
+            { // The tank should stop.
+                controlScript.Stop_Flag = true;
+                controlScript.L_Input_Rate = 0.0f;
+                controlScript.R_Input_Rate = 0.0f;
+                controlScript.Turn_Brake_Rate = 0.0f;
+                controlScript.Pivot_Turn_Flag = false;
+                return;
+            }
+            else
+            { // The tank should be driving.
+                controlScript.Stop_Flag = false;
+            }
+
+            // In case of going straight.
+            if (horizontal == 0.0f)
+            { // The tank should be going straight.
+                controlScript.L_Input_Rate = -vertical;
+                controlScript.R_Input_Rate = vertical;
+                controlScript.Turn_Brake_Rate = 0.0f;
+                controlScript.Pivot_Turn_Flag = false;
+                return;
+            }
+
+            // In case of pivot-turn.
+            if (controlScript.Allow_Pivot_Turn)
+            { // Pivot-turn is allowed.
+                if (vertical == 0.0f && controlScript.Speed_Rate <= controlScript.Pivot_Turn_Rate)
+                { // The tank should be doing pivot-turn.
+                    horizontal *= controlScript.Pivot_Turn_Rate;
+                    controlScript.L_Input_Rate = -horizontal;
+                    controlScript.R_Input_Rate = -horizontal;
+                    controlScript.Turn_Brake_Rate = 0.0f;
+                    controlScript.Pivot_Turn_Flag = true;
+                    return;
+                }
+            }
+            else
+            { // Pivot-turn is not allowed.
+                if (vertical == 0.0f)
+                {
+                    vertical = 0.25f;
+                }
+            }
+
+            // In case of brake-turn.
+            controlScript.Pivot_Turn_Flag = false;
+            Brake_Turn();
+        }
+
+
+        protected virtual void Brake_Turn()
+        {
+            if (horizontal < 0.0f)
+            { // Left turn.
+                controlScript.L_Input_Rate = 0.0f;
+                controlScript.R_Input_Rate = vertical;
+            }
+            else
+            { // Right turn.
+                controlScript.L_Input_Rate = -vertical;
+                controlScript.R_Input_Rate = 0.0f;
+            }
+
+            // Increase the "Turn_Brake_Rate" with the lapse of time.
+            controlScript.Turn_Brake_Rate += (1.0f / brakingTime / Mathf.Abs(controlScript.Speed_Rate)) * Time.deltaTime * Mathf.Sign(horizontal);
+            controlScript.Turn_Brake_Rate = Mathf.Clamp(controlScript.Turn_Brake_Rate, -1.0f, 1.0f);
+        }
+
+    }
+
+}
---
+++ Assets/Physics Tank Maker/C#_Script/Drive_Control_Scripts/Drive_Control_Input_04_Twin_Sticks_CS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using CHS;
+namespace ChobiAssets.PTM
+{
+
+    public class Drive_Control_Input_04_Twin_Sticks_CS : Drive_Control_Input_03_Single_Stick_CS
+    {
+
+        public override void Drive_Input()
+        {
+            
+            if (CHS_Controller.isStarted)
+            {
+                vertical = Input.GetAxis("Vertical");
+                horizontal = Input.GetAxis("Horizontal2");
+            }
+            else
+            {
+                vertical = 0.0f;
+                horizontal = 0.0f;
+            }
+
+            Set_Values();
+        }
+
+    }
+
+}
